bc478ac030ade7b9f17228575f27c2a920df92d8
NasipCOM <salmahunov@mail.ru>
2021-08-23 16:39:07 +0600

    Dialogs refactoring

--- src/components/About/MyPosts/MyPosts.jsx
+++ src/components/About/MyPosts/MyPosts.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
-
+import './MyPosts.css';
 
 const MyPosts = () => {
 
     return (
-        <div>
-            <textarea></textarea>
-            <button>Hello</button>
+        <div className='wrapper'>
+            <div>
+                <div>
+                    <textarea></textarea>
+                </div>
+                <div>
+                    <button>Hello</button>
+                </div>
+            </div>
+            <div>
+                <img alt="" src='https://lh3.googleusercontent.com/8XHt7Rk1U8ET2MErz57rpHc4bYvCW6Ng-kXzn7aXENFr4d0qhtYGCH2TIvWuAO29M20=s180' />
+            </div>
+
         </div>
     );
 }
--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -20,20 +20,37 @@ const Message = (props) => {
 }
 
 const Blog = () => {
+
+    let dialogsData = [
+        { id: 1, name: 'Nasip' },
+        { id: 2, name: 'Aidar' },
+        { id: 3, name: 'Ramazan' },
+        { id: 4, name: 'Emil' },
+        { id: 5, name: 'Ernest' },
+        { id: 6, name: 'Kadyr' }
+    ]
+
+    const dialogElements = dialogsData
+        .map(dialog => <Dialog name={dialog.name} id={dialog.id} />)
+
+    let messages = [
+        { id: 1, message: 'Hello' },
+        { id: 2, message: 'How are you?' },
+        { id: 3, message: 'How is it going?' },
+        { id: 4, message: 'Great job' }
+    ]
+
+    const messagesElement = messages.map((message) => {
+        return <Message message={message.message} />
+    })
+
     return (
         <div className='dialogs'>
             <div className="dialogItems">
-                <Dialog name='Nasip' id='2' />
-                <Dialog name='Aidar' id='2' />
-                <Dialog name='Ramazan' id='2' />
-                <Dialog name='Emil' id='2' />
-                <Dialog name='Nasip' id='2' />
+                {dialogElements}
             </div>
             <div className="messages">
-                <Message message='Hello' />
-                <Message message='How are you?' />
-                <Message message='How is it going?' />
-                <Message message='Great job' />
+                {messagesElement}
             </div>
         </div>
     );
