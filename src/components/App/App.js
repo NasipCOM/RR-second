a126e3effb9141ba9c2473785ce7550d2338669a
NasipCOM <salmahunov@mail.ru>
2022-06-20 18:04:22 +0200

    __

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,14 +2,15 @@ import React from 'react';
 import Head from '../Head/Head';
 import Navbar from '../Navbar/Navbar';
 import Home from '../Home/Home';
-
-import './App.css';
-import { BrowserRouter, Route } from 'react-router-dom';
 import About from '../About/About';
 import Blog from '../Blog/Blog';
+import { BrowserRouter, Route } from 'react-router-dom';
+import './App.css';
+
+
 
 const App = (props) => {
-    console.log(props)
+
     const { profilePage, messagesPage } = props.state;
 
     return (
